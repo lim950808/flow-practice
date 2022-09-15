4c9c0c077be1b5d2515aee0107fc9790a20a0b61
lim950808 <lim080895@gmail.com>
2022-09-15 20:29:17 +0900

    feat: Print Fibonacci if num is 1, 2, or more than 3

--- Fibonacci.java
+++ Fibonacci.java
@@ -3,6 +3,10 @@ public static int Fibonacci(int num) {
 
 	if(num == 1) {
 		result = 1;
+	} else if (num == 2) {
+		result = 1;
+	} else if (num >= 3) {
+		result = Fibonacci(4) + Fibonacci(5);
 	}
 
 	return result;
